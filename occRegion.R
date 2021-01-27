a4f681c5ccf50541cc16d60e94eb6803737b6e72
Arle <ca13kute@idiv.de>
2021-01-27 13:50:01 +0100

    21/01/2020

--- occRegion.R
+++ occRegion.R
@@ -4,6 +4,6 @@ setwd("/gpfs1/data/idiv_meyer/01_projects/eduardo/sTWIST/Intermediate_steps")
 
 table <- readRDS("GRIIS_amphibians_GBIF_shpID")
 
-table2 <- ddply(table,.(species,year,griisRegion), nrow)
+table2 <- ddply(table,.(species,year,griisRegion,griisRegionID), nrow)
 
 saveRDS(table2,"Occurrence_region_count")
---
+++ prepareDataForFig.R
@@ -0,0 +1,67 @@
+library(plyr);library(rgdal);library(raster)
+
+wd.out <- "/gpfs1/data/idiv_meyer/01_projects/eduardo/sTWIST/Intermediate_steps"
+
+setwd(wd.out)
+table <- readRDS("Occurrence_region_count")
+
+names(table)[5] <- "n"
+
+table2 <- table[which(table$year > 1969),]
+table3 <- table2[which(table2$year < 2020),]
+
+#load griis shp
+wd_shp <- "/gpfs1/data/idiv_meyer/01_projects/eduardo/sTWIST/GRIIS_shp"
+shp <- readOGR("GRIIS_ISO3",dsn=wd_shp)
+reg_match <- shp@data[,c("Region","Region2")]
+
+#add "Region" to table (comes with Region2) to match names in master file
+table4 <- merge(table3,reg_match,by.x = "griisRegion",by.y = "Region2",
+                sort = F, all.x = T)
+
+#create column with species and region info
+table4$sps_reg <- paste0(table4$species,"_",table4$Region)
+
+#load amphibian master file
+setwd("/gpfs1/data/idiv_meyer/01_projects/eduardo/sTWIST")
+master_file <- read.csv("GRIISAmph_FirstRecords_FromHanno4Dec2020_AmphOnly.xlxs.csv")
+
+master_file$sps_reg <- paste0(master_file$Taxon,"_",
+                              master_file$Location)
+
+#eliminate rows combining sps/reg that are not listed in the master file
+
+#create column informing to with lustre the occurrences belong
+table4$lustre <- floor((table4$year - 1970) / 5) + 1
+
+#count sps_reg occurrence in the 5 year period
+table5 <- ddply(table4,.(species,Region,sps_reg,lustre),
+                summarise, n_5y = sum(n))
+
+#eliminate rows with combination sps_reg_n_5y < 10
+table6 <- table5[-which(table5$n_5y < 10),]
+
+#eliminate rows with combination sps_reg not contained in the master_file
+table7 <- table6[which(table6$sps_reg %in% master_file$sps_reg),]
+
+#count how many periods of five years per region have at least 10 rec
+table8 <- ddply(table7,.(Region),nrow)
+
+#make a new table for the plot
+mf <- ddply(master_file,.(Location,locationID),nrow)
+names(mf)[3] <- "n_species"
+
+#include table 8 info in mf
+mf2 <- merge(mf,table8,by.x = "Location",by.y = "Region",
+             sort = F, all.x = T)
+mf2$V1[which(is.na(mf2$V1))] <- 0
+
+
+wd <- "I:/MAS/04_personal/Eduardo/sTWIST/GRIIS_shp"
+
+library(raster);library(rgdal)
+
+shp <- readOGR("GRIIS_ISO3",dsn=wd)
+
+shp@data
+head(shp@data)
