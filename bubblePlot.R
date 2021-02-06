c71e1c66eef35965a8333bf3de7a837ce19df3bb
Arle <ca13kute@idiv.de>
2021-02-06 11:16:45 +0100

    06.02.2021

--- bubblePlot.R
+++ bubblePlot.R
@@ -1,6 +1,6 @@
-library(raster);library(rgdal);library(rgeos)
+library(raster);library(rgdal);library(rgeos);library(plotfunctions)
 
-setwd("C:/Users/ca13kute/Dropbox/sTWIST (1)")
+setwd("C:/Users/ca13kute/Dropbox/sTWIST")
 
 table <- read.csv("Information_Status_Indicator.csv")
 
@@ -71,14 +71,14 @@ points(x = rep(12,4), y = c(30,24,18,12),
 ### Plot the maps
 
 #load griis_shp
-wd_shp <- "C:/Users/ca13kute/Dropbox/sTWIST (1)/GRIIS_shp"
+wd_shp <- "C:/Users/ca13kute/Dropbox/sTWIST/GRIIS_shp"
 shp <- readOGR("GRIIS_ISO3",dsn = wd_shp)
 shp2 <- gSimplify(shp,0.2,topologyPreserve = T)
 shp2$a <- rep(0,length(shp2))
 shp2@data <- shp@data
 
 # Load world map frame and continent outline
-setwd("C:/Users/ca13kute/Documents/sTWIST")
+setwd("C:/Users/ca13kute/Dropbox/sTWIST")
 
 world <- readRDS("wrld.rds")
 worldmapframe <- readRDS("Worldmapframe.rds")
@@ -92,7 +92,7 @@ plot(shp2)
 plot(worldmapframe,add=T)
 
 # load old griis table
-setwd("C:/Users/ca13kute/Documents/sTWIST")
+setwd("C:/Users/ca13kute/Dropbox/sTWIST")
 old_table <- read.csv("AlienSpecies_MultipleDBs_Masterfile.csv")
 countries <- as.character(unique(old_table$Country))
 
@@ -157,9 +157,68 @@ col_ISI[which(!is.na(shp3$ISI))] <- rgb(40,40,148,
 
 col_ISI[which(col_ISI=="xx")] <- "white"
 
+par(mar=c(2,2,2,2))
+
 plot(shp3,col=col_ISI)
 plot(worldmapframe,add=T)
-plot(shp3[which(is.na(shp3$n_species)),],add=F,density=50)
+plot(shp3[which(is.na(shp3$n_species)),],add=T,density=150)
+
+col_leg <- colorRampPalette(c("white", rgb(40,40,148,
+                                           alpha=255,
+                                           maxColorValue = 255)))
+
+gradientLegend(valRange = c(0, 1), 
+               pos=c(0.3,0.23,0.7,.245),
+               color = col_leg(20), 
+               side = 1,
+               n.seg = 1)
+
+
+### Plot small map
+
+#create vector to populate with the colours
+col_n_sps <- rep("xx",nrow(shp3)) 
+
+#create vector to populate with the transparency
+alpha_n_sps <- shp3$n_species[which(!is.na(shp3$n_species))]/17 * 255
+
+col_n_sps[which(!is.na(shp3$n_species))] <- rgb(135,0,0,
+                                        alpha=alpha_n_sps,
+                                        maxColorValue = 255)
+
+col_n_sps[which(col_n_sps=="xx")] <- "white"
+
+par(mar=c(2,2,2,2))
+
+plot(shp3,col=col_n_sps)
+plot(worldmapframe,add=T)
+plot(shp3[which(is.na(shp3$n_species)),],add=T,density=150)
+
+
+                                           
+                                          
+     
+           
+shp3@data                                
+                                           
+                                                                                 maxColorValue = 255)))
+##################################
+
+
+brasil <- shp[which(shp$Region == "France"),]
+plot(brasil)
+
+
+?gradientLegend
+
+emptyPlot(1,1, main='Test plot', axes=FALSE)
+box()
+# number of segments:
+gradientLegend(valRange=c(-14,14), n.seg=3, pos=c(.2,.3,.5,.5),
+               side=1)
+
+
+
 
 a <- shp3[which(is.na(shp3$n_species)),]
 a@data     
@@ -171,7 +230,8 @@ target_country@data
 
 #check data from the GRIIS old table
 
-grep("Alaska",old_table$Country)
+grep("Greenland",old_table$Country)
+a <- old_table[which(old_table$Country == "Alaska"),]
 
 test <- shp3[1,]
 plot(test,col=b)
